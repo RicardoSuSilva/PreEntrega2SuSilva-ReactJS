709c498690f180a406d9c5bf21fc9ca8ad730d5c
Ricardo Su Silva <richisusilva@hotmail.com>
2023-12-04 00:43:09 -0300

    hay operatividad falta mejorar el estilo CSS o bootstrap

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
     <Routes>
 
-      <Route path={'/'} element={ <ItemListContainer greeting='Catalogo de Productos'/> } />
+      <Route path={'/'} element={ <ItemListContainer/> } />
       <Route path={'/categoria/:id'} element={ <ItemListContainer/> } />
       <Route path={'/item/:id'} element={ <ItemDetailContainer/> } />
       <Route path={'/cart'} element={ <CartWidget/> } />
